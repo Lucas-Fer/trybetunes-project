f83897a0bec1e054a6156fcaf16ce30514b67bc8
Lucas S. Fernandes <fernandes20lucas@gmail.com>
2021-12-09 17:11:10 -0400

    style: estiliza o header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import * as userApi from '../services/userAPI';
 import LoadingMessage from './LoadingMessage';
+import logo from '../images/logo3.png';
+import '../css/header.css';
 
 export default class Header extends Component {
   constructor() {
@@ -10,26 +12,65 @@ export default class Header extends Component {
     this.state = {
       userInfo: '',
       loading: true,
+      image: '',
     };
   }
 
   componentDidMount() {
     userApi.getUser()
-      .then(({ name }) => this.setState({ userInfo: name, loading: false }));
+      .then(({ name, image }) => this.setState({ userInfo: name, image: image,loading: false }));
   }
 
   render() {
-    const { state: { userInfo, loading } } = this;
+    const { state: { userInfo, image, loading } } = this;
     return (
       <header data-testid="header-component">
-        Sou o header
-        {loading ? (
-          <LoadingMessage />
-        ) : (<span data-testid="header-user-name">{userInfo}</span>)}
-        <br />
-        <Link data-testid="link-to-search" to="/search">Pesquisa</Link>
-        <Link data-testid="link-to-favorites" to="/favorites">Músicas favoritas</Link>
-        <Link data-testid="link-to-profile" to="/profile">Perfil</Link>
+        <div id="header-user-logo">
+          <img
+            src={logo}
+            alt="logo"
+            id="logo"
+          />
+          <h1 id="title">TrybeTunes</h1>
+          {loading ? (
+            <LoadingMessage />
+          ) : (
+              <span
+                id="header-user-info"
+                data-testid="header-user-name">
+                <img
+                  src={image}
+                  className="user-logo"
+                  alt="perfil-user"
+                />
+                {userInfo}
+            </span>
+          )}
+        </div>
+
+        <div id="header-links">
+          <div className="header-link">
+            <Link
+              className="link-decoration"
+              data-testid="link-to-favorites"
+              to="/favorites">Músicas favoritas
+            </Link>
+          </div>
+          <div className="header-link">
+            <Link
+              className="link-decoration"
+              data-testid="link-to-search"
+              to="/search">Pesquisar músicas
+            </Link>
+          </div>
+          <div className="header-link">
+            <Link
+              className="link-decoration"
+              data-testid="link-to-profile"
+              to="/profile">Visualizar Perfil
+            </Link>
+          </div>
+        </div>
       </header>
     );
   }
